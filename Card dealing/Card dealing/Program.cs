b6dbf6d94d3ea4661fb77b5113d70d2e0caf3319
alexwright <lutherwright8925@yahoo.co.uk>
2021-05-18 16:24:47 +0100

    * Card dealing.dll: * Card dealing.pdb: * Card dealing.dll: * Card dealing.pdb: * Card dealing.csprojAssemblyReference.cache:
    * Program.cs: just added some tiding up

--- Card dealing/Card dealing/Program.cs
+++ Card dealing/Card dealing/Program.cs
@@ -14,6 +14,9 @@ namespace Card_dealing
         public static int count;
         static void Main(string[] args)
         {
+            //makes the human and computer objects
+            Human player1 = new Human();
+            Computer computer1 = new Computer();
             // goes over to the deck class creating an array as deck1
             Deck deck1 = new Deck();
             // once the array is created goes over to the shuffle method in the deck class and shuffles our array
@@ -38,7 +41,7 @@ namespace Card_dealing
                     _player(deck1);
                     _computer(deck1);
                     //calls the play method
-                    Play(deck1);
+                    Play(deck1, player1, computer1);
                     break;
                 }
                 // ends program if user does not want anymore cards
@@ -73,11 +76,8 @@ namespace Card_dealing
             ComputersCards = new Hand(deck1);
         }
         //play method for playing the game
-        public static Card Play(Deck deck1)
+        public static Card Play(Deck deck1, Human player1, Computer computer1)
         {
-            //makes the human and computer objects
-            Human player1 = new Human();
-            Computer computer1 = new Computer();
             //for loop so it loops five times as there are five rounds
             for (int i = 0; i < 5; i++)
             {
@@ -89,7 +89,7 @@ namespace Card_dealing
                     if (answer == "1")
                     {
                         //outputs each players cards and checks the totals to see wether it needs to increase score for which one or if it was a draw
-                        Console.WriteLine($"\nYour cards are: {PlayersHand.newHand[(i*2)]} {PlayersHand.newHand[(i*2)+1]}");
+                        Console.WriteLine($"\nYour cards are: {PlayersHand.newHand[(i * 2)]} {PlayersHand.newHand[(i * 2) + 1]}");
                         Console.WriteLine($"The computers cards are: {ComputersHand.newHand[i * 2]} {ComputersHand.newHand[(i * 2) + 1]}");
                         if (((PlayersHand.newHand[(i * 2)]).face + (PlayersHand.newHand[(i * 2) + 1]).face) > ((ComputersHand.newHand[i * 2]).face + (ComputersHand.newHand[(i * 2) + 1]).face))
                         {
@@ -97,6 +97,10 @@ namespace Card_dealing
                             Human.humanID();
                             player1.increasescore(Counter);
                             Console.WriteLine($"Players score is: {player1.score}");
+                            if (Counter > 0)
+                            {
+                                Counter = 0;
+                            }
 
                         }
                         if (((PlayersHand.newHand[(i * 2)]).face + (PlayersHand.newHand[(i * 2) + 1]).face) < ((ComputersHand.newHand[i * 2]).face + (ComputersHand.newHand[(i * 2) + 1]).face))
@@ -105,22 +109,18 @@ namespace Card_dealing
                             Computer.computerID();
                             computer1.increasescore(Counter);
                             Console.WriteLine($"Computers score is: {computer1.score}");
+                            if (Counter > 0)
+                            {
+                                Counter = 0;
+                            }
                         }
                         //if its a draw it checks to see if the next round would make a difference to who wins if not outputs this if so increases a count so that the correct points can be awarded
                         if (((PlayersHand.newHand[(i * 2)]).face + (PlayersHand.newHand[(i * 2) + 1]).face) == ((ComputersHand.newHand[i * 2]).face + (ComputersHand.newHand[(i * 2) + 1]).face))
                         {
-                            
                             if (i == 4)
                             {
-                                if ((player1.score > 2)||(computer1.score > 2))
-                                {
-                                    Console.WriteLine("Looks like the last round was a draw but it doesnt change the winner");
-                                }
-                                else
-                                {
-                                    Console.WriteLine($"\nLooks like it was a draw whoever wins the next round gets all the points!!!");
-                                    count++;
-                                }
+                                Console.WriteLine($"\nLooks like it was a draw whoever wins the next round wins the game!!!");
+                                Draw(deck1, player1, computer1);
                             }
                             else
                             {
@@ -149,6 +149,7 @@ namespace Card_dealing
                     }
                 }
             }
+            Draw(deck1, player1, computer1);
             // outputs the winner if the game is finished and not a draw
             Console.WriteLine($"\n\nThe final scores are: \n {computer1.score} points to the computer \n {player1.score} points to the player ");
             if (computer1.score > player1.score)
@@ -159,38 +160,52 @@ namespace Card_dealing
             {
                 Console.WriteLine("\nYou have won the game!!");
             }
+            return null;
+        }
+        public static Card Draw(Deck deck1, Human player1, Computer computer1)
+        {
             // if the game is a draw will output another card for each player and whichever is biggest wins the game
-            if ((computer1.score == player1.score) && (count == 1))
+            while (true)
             {
                 Console.WriteLine($"Looks like its a draw you both got {player1.score} points, Would you like to deal another card each from the remaining deck? 1 for yes 2 for no.");
                 string ans = Console.ReadLine();
                 if (ans == "1")
                 {
-                    while (true)
+                    //only loops if its a draw again
+                    for (int i = 0; i < 3; i++)
                     {
-                        //only loops if its a draw again
-                        for (int i = 0; i < 10; i++)
+                        Console.WriteLine($"\nYour card is: {PlayersHand.newHand2[i]}");
+                        Console.WriteLine($"The computers card is: {ComputersHand.newHand2[i]}");
+                        if ((PlayersCards.newHand2[i].face) < (ComputersCards.newHand2[i].face))
                         {
-                            Console.WriteLine($"\nYour card is: {PlayersHand.newHand2[i]}");
-                            Console.WriteLine($"The computers card is: {ComputersHand.newHand2[i]}");
-                            if ((PlayersCards.newHand2[i].face) > (ComputersCards.newHand2[i].face))
-                            {
-                                Console.WriteLine($"Well done you have won the game!");
-                                break;
-                            }
-                            if ((PlayersCards.newHand2[i].face) < (ComputersCards.newHand2[i].face))
-                            {
-                                computer1.increasescore(Counter);
-                                Console.WriteLine($"Oh no looks like the computer won :(");
-                                break;
-                            }
-                            if ((PlayersCards.newHand2[i].face) == (ComputersCards.newHand2[i].face))
-                            {
-                                Console.WriteLine("A draw again!! Another card will be drawn.");
-                                continue;
-                            }
+                            player1.increasescore(Counter);
+                            Console.WriteLine($"Well done you have won the game!");
+                            i = 2;
+                            
+                        }
+                        if ((PlayersCards.newHand2[i].face) > (ComputersCards.newHand2[i].face))
+                        {
+                            computer1.increasescore(Counter);
+                            Console.WriteLine($"Oh no looks like the computer won :(");
+                            i = 2;
+                            
+                        }
+                        if ((PlayersCards.newHand2[i].face) == (ComputersCards.newHand2[i].face))
+                        {
+                            Console.WriteLine("A draw again!! Another card will be drawn.");
+                            
                         }
                     }
+                    break;
+                }
+                if (ans == "2")
+                {
+                    Console.WriteLine("Thanks and goodbye");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Not valid input");
                 }
             }
             return null;
